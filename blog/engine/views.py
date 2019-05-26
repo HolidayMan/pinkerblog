a4a9ec6c5cf704e627fecef882d809be0b88e9eb
HolidayMan <lesha.pitenko791@gmail.com>
2019-05-26 23:36:19 +0300

    added PostDetail class

--- blog/engine/views.py
+++ blog/engine/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
+from .models import Post, Tag
 
 def index(request):
-    return render(request, 'engine/index.html')
+    posts = list(Post.objects.all())[-5:]
+    return render(request, 'engine/index.html', context={"posts": posts})
 
 
+class PostDetail(View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(request, "engine/post_detail.html", context={"post": post})
