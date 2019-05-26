86df62a48035b4c91eedebba0418055a5c15b1c8
HolidayMan <lesha.pitenko791@gmail.com>
2019-05-26 23:35:54 +0300

    added Post and Tag models

--- blog/engine/models.py
+++ blog/engine/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.shortcuts import reverse
 
-# Create your models here.
+class Post(models.Model):
+    slug = models.SlugField("Slug of post", max_length=15)
+    title = models.CharField("Title of post", max_length=40)
+    body = models.CharField("Text of post", max_length=3000)
+    date = models.DateTimeField("Date", auto_now_add=True)
+
+
+    def get_absolute_url(self):
+        return reverse("post_detail_url", kwargs={"slug": self.slug})
+    
+
+
+    def __str__(self):
+        return "slug: {}, title: {}, text: {}, date: {}".format(self.slug, self.title, self.text, self.date)
+
+
+class Tag(models.Model):
+    slug = models.SlugField("Slug of post", max_length=15)
+    title = models.CharField("Title of post", max_length=40)
+    post = models.ManyToManyField(Post)
+    date = models.DateTimeField("Date", auto_now_add=True)
+    
+    def __str__(self):
+        return "slug: {}, title: {}, post: {}, date: {}".format(self.slug, self.title, self.post, self.date)
