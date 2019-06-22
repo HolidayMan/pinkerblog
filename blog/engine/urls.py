815135d94eccaef3a0506120239763e3ed985dbb
HolidayMan <lesha.pitenko791@gmail.com>
2019-06-22 18:50:00 +0300

    added new urls

--- blog/engine/urls.py
+++ blog/engine/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('post/<str:slug>/', PostDetail.as_view(), name="post_detail_url"),
+    path('tag/create/', TagCreate.as_view(), name="tag_create_url"),
+    path('tag/list/', TagList.as_view(), name='tag_list_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name="tag_detail_url"),
     path('', index, name="index_url"),
-    
 ]
