6c302527b5643685ce15221a6cc44ece739cdb36
HolidayMan <lesha.pitenko791@gmail.com>
2019-06-22 18:49:25 +0300

    small changes

--- blog/engine/models.py
+++ blog/engine/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     slug = models.SlugField("Slug of post", max_length=15)
     title = models.CharField("Title of post", max_length=40)
     body = models.CharField("Text of post", max_length=3000)
+    tags = models.ManyToManyField("Tag", blank=True, related_name="posts")
     date = models.DateTimeField("Date", auto_now_add=True)
 
 
@@ -14,14 +15,17 @@ class Post(models.Model):
 
 
     def __str__(self):
-        return "slug: {}, title: {}, text: {}, date: {}".format(self.slug, self.title, self.text, self.date)
+        return "slug: {}, title: {}, date: {}".format(self.slug, self.title, self.date)
 
 
 class Tag(models.Model):
-    slug = models.SlugField("Slug of post", max_length=15)
+    slug = models.SlugField("Slug of post", max_length=15, unique=True)
     title = models.CharField("Title of post", max_length=40)
-    post = models.ManyToManyField(Post)
     date = models.DateTimeField("Date", auto_now_add=True)
     
+    def get_absolute_url(self):
+        return reverse("tag_detail_url", kwargs={"slug": self.slug})
+
+
     def __str__(self):
-        return "slug: {}, title: {}, post: {}, date: {}".format(self.slug, self.title, self.post, self.date)
+        return "slug: {}, title: {}, date: {}".format(self.slug, self.title, self.date)
