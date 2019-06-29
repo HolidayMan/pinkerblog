8d5cc75ce68723b7ee60b56ec1575a6dd4e7a940
HolidayMan <lesha.pitenko791@gmail.com>
2019-06-29 22:38:12 +0300

    small changes

--- blog/engine/urls.py
+++ blog/engine/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('post/create/', PostCreate.as_view(), name="post_create_url"),
     path('post/<str:slug>/', PostDetail.as_view(), name="post_detail_url"),
     path('tag/create/', TagCreate.as_view(), name="tag_create_url"),
     path('tag/list/', TagList.as_view(), name='tag_list_url'),
