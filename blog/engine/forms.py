3ec4a3732605ad5240579745e83f6b53de5a8ef1
HolidayMan <lesha.pitenko791@gmail.com>
2019-06-29 22:37:06 +0300

    created PostCreateForm

--- blog/engine/forms.py
+++ blog/engine/forms.py
@@ -4,7 +4,36 @@ from django.core.exceptions import ValidationError
 
 class PostCreateForm(forms.Form):
     title = forms.CharField(max_length=50)
-    body = None
+    slug = forms.SlugField(max_length=30)
+    body = forms.CharField(widget=forms.Textarea)
+    tags = forms.TypedMultipleChoiceField(choices=[(tag.slug, tag.title) for tag in Tag.objects.all()], coerce=lambda slug: Tag.objects.get(slug__iexact=slug), required=False)
+
+    title.widget.attrs.update({'class': 'form-control'})
+    slug.widget.attrs.update({'class': 'form-control'})
+    body.widget.attrs.update({'class': 'form-control'})
+    tags.widget.attrs.update({'class': 'form-control'})
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Slug can\'t be named as "create".')
+        if Post.objects.filter(slug__iexact=new_slug).exists():
+            raise ValidationError("Slug alredy exists.")
+
+        return new_slug
+    
+
+    def save(self):
+        new_post = Post.objects.create(
+            slug=self.cleaned_data['slug'],
+            title=self.cleaned_data['title'],
+            body=self.cleaned_data['body']
+        )
+        print(self.cleaned_data['tags'])
+        new_post.tags.add(*self.cleaned_data['tags'])
+        return new_post
+
 
 
 class TagCreateForm(forms.Form):
