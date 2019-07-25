cd7d6ae6add3ef2abf0db3ad7763d79a4caa9183
HolidayMan <lesha.pitenko791@gmail.com>
2019-07-25 09:09:40 +0300

    registered Post and Tag models

--- blog/engine/admin.py
+++ blog/engine/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+def truncate_words(string, num):
+    string = string.split()
+    if len(string) > num:
+        return " ".join(string[:num]) + " ..."
+    return " ".join(string)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("slug", "title", "display_body", "display_tags", "display_author", "date")
+
+    def display_author(self, obj):
+        return obj.author
+    display_author.short_description = "Author"
+
+    def display_body(self, obj):
+        return truncate_words(obj.body, 15)
+    display_body.short_description = "Body"
+
+    def display_tags(self, obj):
+        return ", ".join(tag.title for tag in obj.tags.all())
+    display_tags.short_description = "Tags"
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("slug", "title", "display_posts", "date")
+    
+    def display_posts(self, obj):
+        return ", ".join(post.title for post in obj.posts.all())
+    display_posts.short_description = "Realated posts"
