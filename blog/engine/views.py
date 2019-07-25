7c1a61deb3c6f2d0bd7f2e6723def1bb16c4c40b
HolidayMan <lesha.pitenko791@gmail.com>
2019-07-26 01:04:02 +0300

    checking permission for post edit and delete also small changes

--- blog/engine/views.py
+++ blog/engine/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import View
-from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from .models import Post, Tag
-from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectEditMixin, ObjectDeleteMixin
-from .forms import TagForm, PostForm
+from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectEditMixin, ObjectDeleteMixin, check_permissions
+from .forms import TagForm, PostForm, PostCreateForm
 
 
 class PostList(View):
     def get(self, request):
         posts = Post.objects.all()
-        page = request.GET.get('page') if request.GET.get('page') else 1
+        page_num = request.GET.get('page', 1)
         paginator = Paginator(posts, 3)
-        return render(request, 'engine/posts_list.html', context={"page": paginator.page(page)})
+        page = paginator.page(page_num)
+        return render(request, 'engine/posts_list.html', context={"page": page})
 
 
 class PostListAll(View):
@@ -32,9 +33,19 @@ class TagDetail(ObjectDetailMixin, View):
     template = "engine/tag_detail.html"
 
 
-class PostCreate(ObjectCreateMixin, View):
-    form = PostForm
-    template = 'engine/post_create.html'
+class PostCreate(LoginRequiredMixin, View):
+    def get(self, request):
+        form = PostCreateForm
+        return render(request, 'engine/post_create.html', context={'form': form})
+    
+    def post(self, request):
+        print(request.POST)
+        bound_form = PostCreateForm(request.user, request.POST)
+        if bound_form.is_valid():
+            post = bound_form.save()
+            return redirect(post)
+        return render(request, 'engine/post_create.html', context={'form': bound_form})
+
 
 class TagList(View):
     def get(self, request):
@@ -42,28 +53,56 @@ class TagList(View):
         return render(request, 'engine/tag_list.html', context={'tags': tags})
 
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form = TagForm
     template = 'engine/tag_create.html'
 
 
-class PostEdit(ObjectEditMixin, View):
-    model = Post
-    form = PostForm
-    template = 'engine/post_edit.html'
+class PostEdit(LoginRequiredMixin, View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        if check_permissions(request.user, post):
+            bound_form = PostForm(instance=post)
+            return render(request, 'engine/post_edit.html', context={'form': bound_form, 'post': post})
+        return render(request, 'errors/forbidden.html', status=403)
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        if check_permissions(request.user, post):
+            bound_form = PostForm(request.POST, instance=post)
+            if bound_form.is_valid():
+                bound_form.save()
+                return redirect(post)
+            return render(request, 'engine/post_edit.html', context={'form': bound_form, 'post': post})
+        return render(request, 'errors/forbidden.html', status=403)        
+
     
 
-class PostDelete(ObjectDeleteMixin, View):
-    model = Post
+    
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     template = "engine/post_delete_form.html"
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        if check_permissions(request.user, post):
+            return render(request, self.template, context={'post': post})
+        return render(request, 'errors/forbidden.html')
+
+    def post(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        if check_permissions(request.user, post):
+            post.delete()
+            return redirect("posts_list_url")
+        return render(request, 'errors/forbidden.html')
 
 
-class TagEdit(ObjectEditMixin, View):
+
+class TagEdit(LoginRequiredMixin, ObjectEditMixin, View):
     model = Tag
     form = TagForm
     template = 'engine/tag_edit.html'
     
 
-class TagDelete(ObjectDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Tag
     template = "engine/tag_delete_form.html"
